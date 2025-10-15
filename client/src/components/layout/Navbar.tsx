85964e7e69ec57e3a0684ecb8c3f33fd836581c5
Polsander <oerdmann@uwaterloo.ca>
2025-10-14 20:09:02 -0400

    edited coloring and format

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -10,10 +10,10 @@ export const Navbar = () => {
   }
 
   return (
-    <nav className='px-16 py-5 flex bg-bg grid grid-cols-2 border-b-1 border-faded items-center'>
+    <nav className='px-16 py-5 flex bg-bg grid grid-cols-2 border-b-1 border-outline items-center'>
       <h4 className='font-bold grid-row text-text flex items-center text-2xl'><img className='h-[3em] w-[3em] pr-2' src={logo}/>Vector Architect</h4>
       <div className='flex justify-end'>
-        <button className='' onClick={handleThemeChange}><TbSunMoon className='w-5 h-5'/></button>
+        <button className='standard' onClick={handleThemeChange}><TbSunMoon className='w-5 h-5'/></button>
       </div>
     </nav>
   )
--- client/src/components/layout/Sidebar.tsx
+++ client/src/components/layout/Sidebar.tsx
@@ -14,7 +14,7 @@ export const Sidebar = () => {
     return (
         <div className={` h-full flex`}>
             <div className={`pt-6 pl-6 ${styles.sidebar} ${hideSidebar ? styles.collapsed : ''}`}>
-                <h3 className='font-bold pb-3'>Category</h3>
+                <h3 className='font-bold pb-3 text-text'>Category</h3>
                 <ul className={styles.categories}>
                     <li>Furniture</li>
                     <li>Vehicles</li>
@@ -24,7 +24,7 @@ export const Sidebar = () => {
             </div>
 
             <div className='flex flex-col bg-side-bg max-w-3'>
-                <div className={`h-45/100 border-r-1 border-faded`}>
+                <div className={`h-45/100 border-r-1 border-outline`}>
                 </div>
                 <div className={`h-10/100 ${styles.mid_section}`} onClick={handleToggleSidebar} onDrag={handleToggleSidebar}>
                     {!hideSidebar ?
@@ -32,7 +32,7 @@ export const Sidebar = () => {
                         <AiFillCaretRight />
                     }
                 </div>
-                <div className={`h-45/100 border-r-1 border-faded`}>
+                <div className={`h-45/100 border-r-1 border-outline`}>
                 </div>
             </div>
 
